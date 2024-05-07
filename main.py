6a25a0fa59f394759fd4a418a9193af59bb15375
Broys42 <boxforanswers@mail.ru>
2024-05-08 01:23:34 +0300

    Added header

--- main.py
+++ main.py
@@ -1,28 +1,71 @@
 import flet as ft
 
-def main(page: ft.Page):
-    t = ft.Tabs(
-        selected_index=0,
-        animation_duration=0,
-        tabs=[
-            ft.Tab(
-                text="Tab 1",
-                content=ft.Container(
-                    content=ft.Text("This is Tab 1"), alignment=ft.alignment.center
-                ),
-            ),
-            ft.Tab(
-                tab_content=ft.Icon(ft.icons.SEARCH),
-                content=ft.Text("This is Tab 2"),
-            ),
-            ft.Tab(
-                text="Tab 3",
-                icon=ft.icons.SETTINGS,
-                content=ft.Text("This is Tab 3"),
-            ),
-        ],
-        expand=1,
-    )
-    page.add(t)
+class Header():
+    def __init__(self):
+        self.selected_index=0
 
-ft.app(target=main)
+    def main(self, page: ft.Page):
+        def change_selected_index(e, key):
+            print(page.width)
+            for index, tab in enumerate(self.tabs):
+                if index == key:
+                    tab.style.color[ft.MaterialState.DEFAULT] = "#e31f2d"
+                else:
+                    tab.style.color[ft.MaterialState.DEFAULT] = "black"
+            page.update()
+
+
+        self.kfc_logo = ft.Image(src=f"/images/kfc_logo.png")
+
+        self.menu_tab = ft.TextButton(
+            text="Меню",
+            style=ft.ButtonStyle(
+            color={
+                ft.MaterialState.HOVERED: "#e31f2d",
+                ft.MaterialState.DEFAULT: "#e31f2d"
+            }),
+            on_click=lambda e: change_selected_index(e=e, key=0)
+        )
+
+        self.users_tab = ft.TextButton(
+            text="Пользователи",
+            style=ft.ButtonStyle(
+            color={
+                ft.MaterialState.HOVERED: "#e31f2d",
+                ft.MaterialState.DEFAULT: "black"
+            }),
+            on_click=lambda e: change_selected_index(e=e, key=1)
+        )
+
+        self.turnON_edit_btn = ft.TextButton("Включить редактирование")
+
+        self.tabs=[self.menu_tab, self.users_tab]
+        self.container = ft.Container(
+            bgcolor = None
+        )
+
+        self.header_tabs = ft.Row(
+            height=30,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            controls=[
+                self.kfc_logo,
+                self.menu_tab,
+                self.users_tab
+                ]
+            )
+
+        self.header = ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            controls=[
+                ft.Row(),
+                self.header_tabs,
+                self.turnON_edit_btn
+            ]
+        )
+
+        page.add(self.header)
+
+ft.app(
+    target=Header().main,
+    assets_dir="assets"
+)
