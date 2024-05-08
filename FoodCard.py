08eea580f5235a9fb16d39561eedc8a301f0d021
Broys42 <boxforanswers@mail.ru>
2024-05-08 19:33:14 +0300

    Added fonts, update header

--- FoodCard.py
+++ FoodCard.py
@@ -6,30 +6,48 @@ class FoodCard():
         pass
 
     def main(self, page: ft.Page):
+        page.fonts = {
+            "SF Pro Display Black": "/fonts/SF-Pro-Display-Black.otf",
+            "SF Pro Display Bold": "/fonts/SF-Pro-Display-Bold.otf",
+            "SF Pro Display Heavy": "/fonts/SF-Pro-Display-Heavy.otf",
+            "SF Pro Display Light": "/fonts/SF-Pro-Display-Light.otf",
+            "SF Pro Display Medium": "/fonts/SF-Pro-Display-Medium.otf",
+            "SF Pro Display Regular": "/fonts/SF-Pro-Display-Regular.otf",
+            "SF Pro Display Semibold": "/fonts/SF-Pro-Display-Semibold.otf",
+            "SF Pro Display Ultralight": "/fonts/SF-Pro-Display-Ultralight.otf",
+        }
+
         foodCard = self.container = ft.Container(
             ft.Column(
+                spacing=30,
                 alignment=ft.MainAxisAlignment.SPACE_EVENLY,
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                 width=250,
-                height=250,
+                height=500,
                 controls=[
                     ft.Image(
-                        src=f"/images/kfc_logo.png"
+                        src=f"/images/food/chicken_cake.png"
                     ),
                     ft.Column(
+                        spacing=30,
                         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                         controls=[
                             ft.Text(
-                                value="Название блюда"
+                                text_align=ft.TextAlign.CENTER,
+                                value="Маэстро чизбургер делюкс hot",
+                                size=20,
+                                font_family="SF Pro Display Medium",
                             ),
                             ft.Row(
                                 alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                                 controls=[
                                     ft.Text(
-                                        value="1000",
+                                        value="1000" + "₽",
+                                        size=20,
+                                        font_family="SF Pro Display Bold",
                                     ),
                                     ft.FilledButton(
-                                        text="В корзину",
+                                        text="Редактировать",
                                         style=ft.ButtonStyle(
                                             shape=ft.RoundedRectangleBorder(
                                                 radius=10),
--- Header.py
+++ Header.py
@@ -6,7 +6,6 @@ class Header():
         self.selected_index = 0
 
     def main(self, page: ft.Page):
-        self.edit_flag = False
 
         def change_selected_index(e, key):
             for index, tab in enumerate(self.tabs):
@@ -38,32 +37,12 @@ class Header():
             on_click=lambda e: change_selected_index(e=e, key=1)
         )
 
-        self.turnON_edit_btn = ft.FilledButton(
-            text="Включить редактирование",
-            style=ft.ButtonStyle(
-                shape=ft.StadiumBorder(),
-                bgcolor="#e31f2d"
-            ),
-            on_click=lambda e: click_turnON_edit_btn(e=e)
-        )
-
-        def click_turnON_edit_btn(e: ft.ControlEvent):
-            if self.edit_flag:
-                self.edit_flag = False
-                e.control.text = "Включить редактирование"
-                page.update()
-            else:
-                self.edit_flag = True
-                e.control.text = "Отключить редактирование"
-                page.update()
-
         self.tabs = [self.menu_tab, self.users_tab]
 
-        self.header_tabs = ft.Row(
+        self.header= ft.Row(
             height=30,
             alignment=ft.MainAxisAlignment.CENTER,
             spacing=20,
-            width=150,
             controls=[
                 self.kfc_logo,
                 self.menu_tab,
@@ -71,12 +50,4 @@ class Header():
             ]
         )
 
-        self.header = ft.Row(
-            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-            controls=[
-                ft.Text(),
-                self.header_tabs,
-                self.turnON_edit_btn
-            ]
-        )
         page.add(self.header)
